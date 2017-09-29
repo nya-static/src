ef73dfc31d5dd16bd36780476481f6a871911b41
nya-static <32368466+nya-static@users.noreply.github.com>
2017-09-29 16:01:53 +0800

    Update rm-aliyun-service.sh

--- sh/rm-aliyun-service.sh
+++ sh/rm-aliyun-service.sh
@@ -4,3 +4,14 @@ sudo curl -sSL https://raw.githubusercontent.com/MeowLove/AlibabaCloud-CentOS7-P
 sudo rm -rf /usr/local/aegis
 sudo rm /usr/sbin/aliyun-service
 sudo rm /lib/systemd/system/aliyun.service
+iptables -I INPUT -s 140.205.201.0/28 -j DROP
+iptables -I INPUT -s 140.205.201.16/29 -j DROP
+iptables -I INPUT -s 140.205.201.32/28 -j DROP
+iptables -I INPUT -s 140.205.225.192/29 -j DROP
+iptables -I INPUT -s 140.205.225.200/30 -j DROP
+iptables -I INPUT -s 140.205.225.184/29 -j DROP
+iptables -I INPUT -s 140.205.225.183/32 -j DROP
+iptables -I INPUT -s 140.205.225.206/32 -j DROP
+iptables -I INPUT -s 140.205.225.205/32 -j DROP
+iptables -I INPUT -s 140.205.225.195/32 -j DROP
+iptables -I INPUT -s 140.205.225.204/32 -j DROP
